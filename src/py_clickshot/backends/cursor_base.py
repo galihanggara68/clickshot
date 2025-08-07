69eae71d1576f31e7f4f228f14fbcc2d31d8b511
galihanggara68 <galihanggara68@gmail.com>
2025-08-07 15:06:23 +0700

    fix: [galih] change union None to Optional

--- src/py_clickshot/backends/cursor_base.py
+++ src/py_clickshot/backends/cursor_base.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 class CursorProvider(ABC):
     @abstractmethod
-    def get_position(self) -> tuple[int, int] | None:
+    def get_position(self) -> Optional[tuple[int, int]]:
         pass
